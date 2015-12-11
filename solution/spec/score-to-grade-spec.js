ac7d2c18e944bfe06b057fc6e6418c3ca5fd5c46
LouJia <loujia1991@gmail.com>
2015-12-11 11:02:10 +0800

    [loujia] add basic solution

--- solution/spec/score-to-grade-spec.js
+++ solution/spec/score-to-grade-spec.js
@@ -1,1 +1,33 @@
+"use strict";
+describe("grade calculate", function () {
+
+    it("should return S when input is 99", function () {
+        expect(gradeCalculate(99)).toBe("S");
+    });
+
+    it("should return A when input is 85", function () {
+        expect(gradeCalculate(85)).toBe("A");
+    });
+
+    it("should return B when input is 75", function () {
+        expect(gradeCalculate(75)).toBe("B");
+    });
+
+    it("should return C when input is 65", function () {
+        expect(gradeCalculate(65)).toBe("C");
+    });
+
+    it("should return D when input is 50", function () {
+        expect(gradeCalculate(50)).toBe("D");
+    });
+
+    it("should return Invalid input when input is 101", function () {
+        expect(gradeCalculate(101)).toBe("Invalid input");
+    });
+
+    it("should return Invalid input when input is -1", function () {
+        expect(gradeCalculate(-1)).toBe("Invalid input");
+    });
+
+});
 
--- solution/src/score-to-grade.js
+++ solution/src/score-to-grade.js
@@ -1,1 +1,25 @@
+function gradeCalculate(score) {
+    if(score > 100 || score < 0) {
+        return "Invalid input"
+    }
 
+    if (score >= 90) {
+        return Grade.S;
+    }
+    else if (score >= 80)
+        return Grade.A;
+    else if (score >= 70)
+        return Grade.B;
+    else if (score >= 60)
+        return Grade.C;
+    else
+        return Grade.D;
+}
+
+Grade = {
+    S: "S",
+    A: "A",
+    B: "B",
+    C: "C",
+    D: "D"
+};
